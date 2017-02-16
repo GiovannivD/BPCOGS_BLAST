f9747af3abc7217754ca4e6d3ca057ed977a7f8c
GiovannivD <giovannivandonge@gmail.com>
2017-02-16 15:22:04 +0100

    Add files via upload

--- Blast_Script.py
+++ Blast_Script.py
@@ -1,25 +1,41 @@
 '''BLAST SCRIPT V1.0'''
 import os
 
-def create_db():
-    os.system("formatdb -i Proteomes/Proteoom_Phanerochaete_chrysosporium.fasta"
-              " -pF")
+def create_db(schimmels):
+    for item in schimmels:
+        db_string = "formatdb -i /Proteoom_" + item + ".fasta -pF"
+        os.system(db_string)
 
-def blast():
+def blast(schimmel_1, schimmel_2, schimmels):
     '''Alle blasts (proteoom tegen proteoom)'''
-    os.system("blastall -i Proteoom_Ashbya_gossypii.fasta -d"
-              "Proteoom_Aspergillus_nidulans.fasta -p blastp -m9 > blasttest")
+
+    for x in range(len(schimmels)):
+        schimmel_1 = schimmels[x]
+        schimmel_2 = schimmels[x+1]
+        blast_string = "blastall -i Proteoom_" + schimmel_1 + ".fasta -d Proteoom_" + schimmel_2 + ".fasta -p blastp -m9 > blast_" + schimmel_1 + "_vs_" + schimmel_2
+        print(blast_string)
+
+
+    #os.system("blastall -i Proteoom_Ashbya_gossypii.fasta -d"
+    #          "Proteoom_Aspergillus_nidulans.fasta -p blastp -m9 > blasttest")
 
 def split():
     '''Haalt alle beste hits uit de blasts en zet deze in een aparte file die
     dmv de naam van de schimmels wordt benoemd.'''
     
     os.system("cat blasttest | awk '{print $1, $2, $11}' | sed 's/# Fields: "
-              "mismatches,/@/g' | awk '/@/{getline; print}' | egrep -v ^# > AshGos_AspNid")
+              "mismatches,/@/g' | awk '/@/{getline; print}' | egrep -v ^# > "
+              "AshGos_AspNid")
 
 def main():
-    create_db()
-    blast()
-    split()
+    schimmel_list = ["Ashbya_gossypii", "Aspergillus_nidulans", "Neurospora_crassa",
+                    "Penicillium_canescens", "Penicillium_raistrickii",
+                    "Phanerochaete_chrysosporium", "Rhizoctonia_solani",
+                    "Saccharomyces_cerevisea", "Trichoderma_atroviride",
+                    "Trichoderma_virens"]
+
+    #create_db(schimmel_list)
+    blast("test_1", "test_2", schimmel_list)
+    #split()
 
 main()
