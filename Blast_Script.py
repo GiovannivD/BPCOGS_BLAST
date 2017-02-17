f57d068b64a2fa9ad96885a337be4c507a21ed7a
GiovannivD <giovannivandonge@gmail.com>
2017-02-17 15:43:48 +0100

    Add files via upload

--- Blast_Script.py
+++ Blast_Script.py
@@ -55,37 +55,36 @@ def hits(b_file_list):
 
 
 def directional_hit(b_file_list):
-    for x in b_file_list:
-        print("1======", b_file_list)
-        x = x.replace('*', '')
-        deel1, deel2 = x[:len(x)/2], x[len(x)/2:]
-        print("D1:", deel1)
-        print("D2:", deel2)
+    while range(len(b_file_list)) > 0:
+        for x in b_file_list:
+            #print("Start:", b_file_list)
+            x = x.replace('*', '')
+            deel1, deel2 = x[:len(x)/2], x[len(x)/2:]
 
-        turn_string = "awk '{print $2, $1}' HITS/hits_" + deel1 + "*" + \
-                      deel2 + ".txt > turned_" + deel1 + "*"+ deel2 + ".txt"
-        b_file_list.remove(deel1 + "*" + deel2)
+            turn_string = "awk '{print $2, $1}' HITS/hits_" + deel1 + "*" + \
+                          deel2 + ".txt > Modified/turned_" + deel1 + "*" + \
+                          deel2 + ".txt"
+            b_file_list.remove(deel1 + "*" + deel2)
 
-        two_column = "awk '{print $1, $2}' HITS/hits_" + deel2 + "*" + \
-                     deel1 + ".txt > twocolumn_" + deel2 + "*" + deel1 + ".txt"
-        b_file_list.remove(deel2 + "*" + deel1)
+            two_column = "awk '{print $1, $2}' HITS/hits_" + deel2 + "*" + \
+                         deel1 + ".txt > Modified/twoCol_" + deel2 + "*" + \
+                         deel1 + ".txt"
+            b_file_list.remove(deel2 + "*" + deel1)
 
-        print(turn_string)
-        print(two_column)
-        os.system(turn_string)
-        os.system(two_column)
+            os.system(turn_string)
+            os.system(two_column)
 
-        duplicaten = "sort turned_" + deel1 + "*" + deel2 + ".txt twocolumn_" + \
-                     deel2 + "*" + deel1 + \
-                     ".txt | awk 'dup[$0]++ == 1' > BDH/bdh_" + deel1 + deel2 \
-                     + ".txt"
+            duplicaten = "sort Modified/turned_" + deel1 + "*" + deel2 + \
+                         ".txt Modified/twoCol_" + deel2 + "*" + deel1 + \
+                         ".txt | awk 'dup[$0]++ == 1' > BDH/bdh_" + deel1 + \
+                         deel2 + ".txt"
 
-        os.system(duplicaten)
-        print("2======", b_file_list)
+            os.system(duplicaten)
+            #print("Eind:", b_file_list)
 
 
 def main():
-    os.system("mkdir -p BLAST HITS BDH")
+    os.system("mkdir -p BLAST HITS BDH Modified")
     schimmel_list = ["Ashbya_gossypii", "Aspergillus_nidulans",
                      "Neurospora_crassa"]
                      #"Penicillium_canescens", "Penicillium_raistrickii",
